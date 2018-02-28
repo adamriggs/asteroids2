1147956d797b214333016aaab856503ded503415
Adam Riggs <phlux1111@gmail.com>
2018-02-28 00:49:47 -0500

    not quite there yet with the controls

--- src/sprites/Ship.js
+++ src/sprites/Ship.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser'
+import Point from 'phaser'
 
 export default class extends Phaser.Sprite {
   constructor ({ game, x, y, asset }) {
@@ -9,39 +10,44 @@ export default class extends Phaser.Sprite {
     this.scale.x = 0.5
     this.scale.y = 0.5
 
+    this.acceleration = 
+
     this.cursors = game.input.keyboard.createCursorKeys()
     //console.log(game.input.keyboard)
-    this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
+
+    // this doesn't work yet
+    this.game.physics.enable(this, Phaser.Physics.ARCADE);
   }
 
   update () {
-    //this.angle += 1
+    // this.angle += 1
 
     if (this.cursors.up.isDown) {
-    	this.y -= 1
-    	//game.physics.arcade.accelerationFromRotation(sprite.rotation, 200, sprite.body.acceleration)
+ 		// this.y -= 1
+ 		// game.physics.arcade.accelerationFromRotation(sprite.rotation, 200, sprite.body.acceleration)
 
-    	//this.game.physics.arcade.accelerationFromRotation(this.rotation, 200, this.body.acceleration)
+ 		this.game.physics.arcade.accelerationFromRotation(this.angle, 200, new Point(1, 1))
     } else {
-    	//sprite.body.acceleration.set(0);
+		// sprite.body.acceleration.set(0);
     }
 
     if (this.cursors.down.isDown) {
-    	this.y += 1
+		this.y += 1
     }
 
     if (this.cursors.left.isDown) {
-    	this.angle -= 1
-    	//sprite.body.angularVelocity = -300;
+		this.angle -= 1
+		// sprite.body.angularVelocity = -300;
     }
 
     if (this.cursors.right.isDown) {
-    	this.angle += 1
-    	//sprite.body.angularVelocity = 300;
+		this.angle += 1
+		// sprite.body.angularVelocity = 300;
     }
 
     if (this.spaceKey.isDown) {
-    	console.log("fire!")
+		console.log("fire!")
     }
 
     this.screenWrap(this)
