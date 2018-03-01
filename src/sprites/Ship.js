3c1ca41af0f864e2cd6ff67d9087805e895c1680
Adam Riggs <phlux1111@gmail.com>
2018-02-28 22:49:57 -0500

    clean up

--- src/sprites/Ship.js
+++ src/sprites/Ship.js
@@ -2,55 +2,53 @@ import Phaser from 'phaser'
 import Point from 'phaser'
 
 export default class extends Phaser.Sprite {
+
   constructor ({ game, x, y, asset }) {
+
     super(game, x, y, asset)
+    
     this.anchor.setTo(0.5)
     this.game = game
 
     this.scale.x = 0.5
     this.scale.y = 0.5
 
-    this.acceleration = 
-
     this.cursors = game.input.keyboard.createCursorKeys()
-    //console.log(game.input.keyboard)
     this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
 
-    // this doesn't work yet
-    this.game.physics.enable(this, Phaser.Physics.ARCADE);
+    this.game.physics.enable(this, Phaser.Physics.ARCADE)
 
-    this.body.drag.set(100);
-    this.body.maxVelocity.set(200);
-    //this.body.acceleration.set(100);
+    this.body.drag.set(100)
+    this.body.maxVelocity.set(200)
   }
 
   update () {
-    // this.angle += 1
+ 
     this.body.angularAcceleration = 0
 
     if (this.cursors.up.isDown) {
- 		//this.y -= 1
- 		//this.body.angularAcceleration -= 200
- 		// game.physics.arcade.accelerationFromRotation(sprite.rotation, 200, sprite.body.acceleration)
 
  		this.game.physics.arcade.accelerationFromRotation(this.rotation, 200, this.body.acceleration)
     } else {
-		this.body.acceleration.set(0);
+		this.body.acceleration.set(0)
     }
 
     if (this.cursors.down.isDown) {
-		//this.y += 1
-		//this.body.angularAcceleration += 200
+		
     }
 
     if (this.cursors.left.isDown) {
-		//this.angle -= 1
-		this.body.angularVelocity = -300;
+
+		this.body.angularVelocity = -300
+
     }else if (this.cursors.right.isDown) {
-		//this.angle += 1
-		this.body.angularVelocity = 300;
+
+		this.body.angularVelocity = 300
+
     } else {
-        this.body.angularVelocity = 0;
+
+        this.body.angularVelocity = 0
+
     }
 
     if (this.spaceKey.isDown) {
--- src/states/Game.js
+++ src/states/Game.js
@@ -9,22 +9,6 @@ export default class extends Phaser.State {
   preload() { }
 
   create() {
-    // const bannerText = 'Phaser + ES6 + Webpack'
-    // let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText, {
-    //   font: '40px Bangers',
-    //   fill: '#77BFA3',
-    //   smoothed: false
-    // })
-
-    // banner.padding.set(10, 16)
-    // banner.anchor.setTo(0.5)
-
-    // this.mushroom = new Mushroom({
-    //   game: this.game,
-    //   x: this.world.centerX,
-    //   y: this.world.centerY,
-    //   asset: 'mushroom'
-    // })
 
     this.ship = new Ship({
       game: this.game,
@@ -40,7 +24,6 @@ export default class extends Phaser.State {
       asset: 'game-crockett'
     })
 
-    // this.game.add.existing(this.mushroom)
     this.game.add.existing(this.ship)
     this.game.add.existing(this.crockett)
   }
