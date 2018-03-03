815781116446700970ab71e915387f11d85eead7
Adam Riggs <phlux1111@gmail.com>
2018-03-02 23:09:55 -0500

    added bullet class

---
+++ src/sprites/Bullet.js
@@ -0,0 +1,27 @@
+import Phaser from 'phaser'
+import { screenWrap } from '../utils'
+
+export default class extends Phaser.Sprite {
+
+  constructor ({ game, x, y, asset }) {
+
+    super(game, x, y, asset)
+    this.anchor.setTo(0.5)
+
+    this.init()
+
+  }
+
+  init () {
+
+    this.scale.x = 0.1
+    this.scale.y = 0.1
+
+  }
+
+  update () {
+    // this.x +=0.1
+    // this.y +=0.5
+    screenWrap(this)
+  }
+}
