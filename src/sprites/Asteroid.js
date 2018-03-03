12d839f94d03272bf5cf93912fe70899f5bb2077
Adam Riggs <adamriggs@ctpadmins-MacBook-Pro.local>
2018-03-02 21:59:11 -0500

    adding asteroids file

--- src/sprites/Asteroid.js
+++ src/sprites/Asteroid.js
@@ -3,14 +3,16 @@ import { screenWrap } from '../utils'
 
 export default class extends Phaser.Sprite {
 
-  var asset;
-
   constructor ({ game, x, y, asset }) {
-
-    this.asset = asset
 
     super(game, x, y, asset)
     this.anchor.setTo(0.5)
+
+    this.init()
+
+  }
+
+  init () {
 
     this.scale.x = 0.1
     this.scale.y = 0.1
